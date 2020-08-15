bbd7a9a512591ba25f46f5ae445162e77f361c96
MainIT <67462191+MainIT@users.noreply.github.com>
2020-08-15 07:54:42 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,24 @@
+clear
+sleep 1
+echo "Installing..."
+clear
+sleep 2
+echo "Installing..."
+clear
+sleep 2
+echo "Installing..."
+clear
+sleep 2
+echo "Installing..."
+clear
+sleep 2
+echo "Installing..."
+clear
+sleep 2
+pkg update && pkg upgrade
+pkg install git
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pip2 install bs4
+python2 updatev3.py
